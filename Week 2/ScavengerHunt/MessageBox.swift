5af32ad33fa2dc5c226f0e6e5c4d349dfe75511a
zee0701 <37773439+zee0701@users.noreply.github.com>
2018-04-12 22:45:29 +1000

    Add files via upload

---
+++ Week 2/ScavengerHunt/MessageBox.swift
@@ -0,0 +1,45 @@
+
+ 
+import Foundation
+import UIKit
+import TTGSnackbar
+
+
+class MessageBox{
+    
+    public static func Show(message: String, title: String, view: UIViewController){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        let dismisAction = UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil)
+        alertController.addAction(dismisAction)
+        view.present(alertController, animated: true, completion: nil)
+    }
+    
+    public static func SetupIndicator(view: UIViewController)->UIActivityIndicatorView{
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+        indicator.hidesWhenStopped = true
+        indicator.center = view.view.center
+        view.view.addSubview(indicator)
+        return indicator
+    }
+    
+    public static func SetupIndicatorWhite(view: UIView)->UIActivityIndicatorView{
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
+        indicator.hidesWhenStopped = true
+        indicator.center = view.center
+        view.addSubview(indicator)
+        return indicator
+    }
+    
+    public static func SetupIndicator(view: UIView)->UIActivityIndicatorView{
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+        indicator.hidesWhenStopped = true
+        indicator.center = view.center
+        view.addSubview(indicator)
+        return indicator
+    }
+    
+    public static func ShowSnackbar(message: String){
+        let bar = TTGSnackbar(message: message, duration: .middle)
+        bar.show()
+    }
+}
